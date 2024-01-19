9d05ca3733a23bc99e8beabb8f999cb7920b237d
Oleg Kytsko <oleg.kytsko@gmail.com>
2024-01-19 13:03:55 +0200

    Added TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("http://127.0.0.1:0")
+        .expect("Failed to bind random port");
+    
+    run(listener)?.await
 }
